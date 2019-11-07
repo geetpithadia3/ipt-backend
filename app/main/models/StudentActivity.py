5f383d08ba631688096e5f0b34eabead128110e2
Karan Chauhan <ckaran40@gmail.com>
2019-11-07 14:43:35 -0500

    Added student activity files
    Added student activity files

--- app/main/models/StudentActivity.py
+++ app/main/models/StudentActivity.py
@@ -1,2 +1,12 @@
+# Model for Activities of User to be tracked
+import datetime
+from app.database import DB
 class StudentActivity(object):
-    
+    def __init__(self,activity):
+        self.skills = activity["skills"]
+        self.companies = activity["companies"]
+        self.created_by = activity["created_by"]
+        self.created_at = datetime.datetime.utcnow()
+
+    def insert(self):    
+        DB.insert(collection='studentactivity', data=self.json())
